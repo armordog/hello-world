8c44d8a22f03134eecfc0c79aea68214259aab16
Wynn Slater <wynn.slater@ul.com>
2016-04-20 10:55:12 -0700

    Explicitly list karma plugins for travis-ci

--- karma.conf.js
+++ karma.conf.js
@@ -52,7 +52,7 @@ module.exports = function(config) {
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
 
-
+    plugins: ['karma-jasmine', 'karma-phantomjs-launcher'],
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
